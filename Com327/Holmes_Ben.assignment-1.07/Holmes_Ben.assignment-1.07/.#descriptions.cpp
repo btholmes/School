29623c7a94e2a9dd58ac002f93ba75f8e8c6b861
btholmes <btholmes@iastate.edu>
2017-03-25 13:15:47 -0500

    Update

--- Com327/Holmes_Ben.assignment-1.07/Holmes_Ben.assignment-1.07/.#descriptions.cpp
+++
@@ -1,1 +0,0 @@
-student@coms327-vm.cs.iastate.edu.2655:1477344421
--- Com327/Holmes_Ben.assignment-1.08/Holmes_Ben.assignment-1.08/dungeon.d
+++
@@ -1,2 +0,0 @@
-dungeon.o: dungeon.cpp dungeon.h heap.h dims.h character.h utils.h \
- descriptions.h dice.h object.h event.h pc.h npc.h macros.h
